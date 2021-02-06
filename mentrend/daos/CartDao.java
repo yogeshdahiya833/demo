c1bcc8ee364253caf51c455ca9b845955b8a813c
Yogesh <78639219+yogeshdahiya833@users.noreply.github.com>
2021-02-06 15:05:35 +0530

    delmentrend

--- mentrend/daos/CartDao.java
+++
@@ -1,9 +0,0 @@
-package com.mentrend.daos;
-
-import com.mentrend.models.Cart;
-
-public interface CartDao {
-	public boolean addCart(Cart cart);
-	public Cart getCartByCustomer(String customerId);
-	public boolean deleteCart(int cartId);
-}
--- mentrend/daos/OrderDao.java
+++
@@ -1,7 +0,0 @@
-package com.mentrend.daos;
-
-import com.mentrend.models.Order;
-
-public interface OrderDao {
-	public boolean makeOrder(Order order);
-}
